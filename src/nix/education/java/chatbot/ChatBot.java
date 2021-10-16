717ae33745cd69cd2a841bb68483bde2b6d94ab3
Maxim621 <parfemax@gmail.com>
2021-10-16 22:15:20 +0300

    ChatBot 2-nd stage

--- src/nix/education/java/chatbot/ChatBot.java
+++ src/nix/education/java/chatbot/ChatBot.java
@@ -1,2 +1,20 @@
-package nix.education.java.chatbot;public class ChatBot {
+package nix.education.java.chatbot;
+
+import java.util.Scanner;
+
+public class ChatBot {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String response;
+
+        System.out.println("Hello! My name is Jarvis.\nI was created in 2021.\nPlease, remind me your name.");
+
+        while (true) {
+            response = scanner.nextLine();
+            if (response.equals("Maxim")) {
+                System.out.println("What a great name you have, Maxim!");
+                break;
+            }
+        }
+    }
 }
