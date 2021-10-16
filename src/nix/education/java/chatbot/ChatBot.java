48b559d11e191eb33cdcfa05b0e470486a6ff61d
Maxim621 <parfemax@gmail.com>
2021-10-16 23:30:45 +0300

    ChatBot 3-rd stage

--- src/nix/education/java/chatbot/ChatBot.java
+++ src/nix/education/java/chatbot/ChatBot.java
@@ -9,10 +9,27 @@ public class ChatBot {
 
         System.out.println("Hello! My name is Jarvis.\nI was created in 2021.\nPlease, remind me your name.");
 
+
         while (true) {
             response = scanner.nextLine();
             if (response.equals("Maxim")) {
                 System.out.println("What a great name you have, Maxim!");
+                break;
+            }
+        }
+
+        System.out.println("Let me guess your age.\nEnter remainders of dividing your age by 3, 5 and 7.");
+
+        int age = 0;
+        int remainder3 = scanner.nextInt();
+        int remainder5 = scanner.nextInt();
+        int remainder7 = scanner.nextInt();
+
+        while (true) {
+            response = scanner.nextLine();
+            if (response.equals("")) {
+                age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+                System.out.println("Your age is " + age + ";" + " that's a good time to start programming!");
                 break;
             }
         }
